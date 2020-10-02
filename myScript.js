a9e76a13ab2112461ab4aa07da96772902df1f77
=mustafa0624 <mustafa.yucel0624@gmail.com>
2020-10-02 23:43:22 +0200

    feat:add hangs Words

---
+++ myScript.js
@@ -0,0 +1,20 @@
+const wordList = ["absurd", "awkward", "clarusway", "funny", "galaxy",
+ "joking", "fullstack", "strength", "puzzling", "kilobyte", "keyhole",
+  "cycle", "loop", "slack", "function", "fixable", "buzzard", "avenue", 
+  "lengths", "monitor", "information"];
+
+let number = Math.floor(Math.random() * (wordList.length -1));
+let randomWord= wordList[number].split("")
+console.log(randomWord)
+
+function q(par){
+    return document.querySelector(par);
+}
+
+function myWord(){
+    
+    
+
+}
+
+  
