0dfc901a7a7f5110a3341acb27225449f4dfebbc
=mustafa0624 <mustafa.yucel0624@gmail.com>
2020-10-03 11:31:11 +0200

    add: second part

--- myScript.js
+++ myScript.js
@@ -7,6 +7,8 @@ let number = Math.floor(Math.random() * (wordList.length -1));
 let randomWord= wordList[number].split("")
 // console.log(randomWord)
 let divList = [];
+let trueWords = [];
+let wrongWords = [];
 
 function q(par){
     return document.querySelector(par);
@@ -19,20 +21,31 @@ function selectWord(){
   randomWord.forEach(item=>{
     let division = document.createElement("div");
     division.classList.add("letter");
-    division.innerText=item;
+    division.innerText = item;
     q(".word").appendChild(division);
     divList.push(division)
-    console.log(division.innerText)
+    // console.log(divList)
+    // console.log(divList.innerText)
     
   })
 }
-
 function wordControl(e){
     
-    divList.forEach(a =>{
-      if(e.key === a){
-        a.style.color = "red"
+    divList.forEach(item =>{
+      
+      if(item.innerText == e.key) {
+        item.style.color = "red";
+        trueWords.push(item.innerText);
+        console.log("true: ",trueWords)
       }
+      else if(!randomWord.includes(e.key) && !wrongWords.includes(e.key)){
+        q(".wrong").style.display = "inline";
+        wrongWords.push(e.key)
+        q(".wrong").innerText += e.key
+        // console.log("wrong: ",wrongWords)
+    
+      }
+     
     })
       
     
